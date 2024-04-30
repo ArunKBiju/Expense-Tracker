fbf584cffa8c9d16e696a174cbdb1970cf9ea077
Arun K Biju <168078825+ArunKBiju@users.noreply.github.com>
2024-04-30 21:52:09 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Signup</title>
+    <!-- Add your CSS link here -->
+</head>
+<body>
+    <form action="register.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        <input type="submit" value="Sign Up">
+    </form>
+</body>
+</html>
